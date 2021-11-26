2dfdd1a6ac0e6eef6e1b1734d1a41e53c0def4cb
Alvis <alvisgrigaluns@gmail.com>
2021-11-26 12:00:44 +0200

    cleaning

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-    <footer>
-
+    <footer class="text-center p-4">
+        Footer
     </footer>
     <script type="text/javascript" src="scripts/script.js"></script>
 </body>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'includes/header.php';
             <a href="#" class="btn btn-white border">Add</a>
             <a href="#" class="btn btn-white border">Mass delete</a>
         <div class="row">
-            <div class="card col-4">
+            <div class="card col-4 p-3">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
                     <label class="form-check-label" for="defaultCheck1"></label>
@@ -26,32 +26,24 @@ include 'includes/header.php';
                     <p class="card-text">Size</p>
                 </div>
             </div>
-            <div class="card col-4">
+            <div class="card col-4 p-3">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
                     <label class="form-check-label" for="defaultCheck1"></label>
                 </div>
                 <div class="card-body text-center">
-                    <?php
-                    /*$products = new product();
-                    $products->showAllProducts();*/
-                    ?>
                     <h4 class="card-title">SKU</h4>
                     <h5 class="card-title">Name</h5>
                     <p class="card-text">Price</p>
                     <p class="card-text">Type</p>
                     <p class="card-text">Size</p>
                 </div>
-            </div><div class="card col-4">
+            </div><div class="card col-4 p-3">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
                     <label class="form-check-label" for="defaultCheck1"></label>
                 </div>
                 <div class="card-body text-center">
-                    <?php
-                    /*$products = new product();
-                    $products->showAllProducts();*/
-                    ?>
                     <h4 class="card-title">SKU</h4>
                     <h5 class="card-title">Name</h5>
                     <p class="card-text">Price</p>
